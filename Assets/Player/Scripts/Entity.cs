d41b7d4a3257e4594cbf73e328952ae0bee98160
Hao <120632700+hcnguyen1@users.noreply.github.com>
2024-02-22 23:44:47 -0800

    Entity script added

---
+++ Assets/Player/Scripts/Entity.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    // Entity stats
+    [SerializeField] public float HP;
+    [SerializeField] public float HPCapacity;
+    [SerializeField] public float shield;
+    [SerializeField] public float shieldCapacity;
+    [SerializeField] public float attackPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
