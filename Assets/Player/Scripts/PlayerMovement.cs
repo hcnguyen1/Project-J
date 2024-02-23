e7b77500fad823e7f25c96bd24753db3f0cfb7b7
Ascinei <alexredford.random@gmail.com>
2024-02-22 23:22:41 -0800

    Tidied up player movement script

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -4,24 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private float vertical;
-
-    private float speed = 8f;
-
+    private float speed = 8f;   
 
     [SerializeField] private Rigidbody2D rb;
 
-
-
     // Update is called once per frame
     void Update()
-    {
-
+    { 
         Vector3 PlayerMovement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         rb.velocity = PlayerMovement * speed;
-        // rb.transform.position += PlayerMovement * speed;
-
     }
 
 }
