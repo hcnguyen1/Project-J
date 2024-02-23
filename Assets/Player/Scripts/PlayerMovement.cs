58f2e682522deb299757503016ccf8a0a2c91bf2
Ascinei <alexredford.random@gmail.com>
2024-02-23 00:34:14 -0800

    Added comments...

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 8f;   
+    // Controls player movement speed (Multiplies velocity by 8).
+    private float speed = 8f;  
 
+    // The player
     [SerializeField] private Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     { 
+        // Vector responsible for player inpud (WASD), and converts that input into horizontal/vertical 3d vector with a nil value for the z-axis.
         Vector3 PlayerMovement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+        // Controls the velocity of the player.
         rb.velocity = PlayerMovement * speed;
     }
 
