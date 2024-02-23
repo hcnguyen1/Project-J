14e6afa8fed986775d3b4fd00b38a560c10713a6
Ascinei <alexredford.random@gmail.com>
2024-02-22 23:13:37 -0800

    Added player and basic player movement

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+
+    private float speed = 8f;
+
+
+    [SerializeField] private Rigidbody2D rb;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 PlayerMovement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        rb.velocity = PlayerMovement * speed;
+        // rb.transform.position += PlayerMovement * speed;
+
+    }
+
+}
