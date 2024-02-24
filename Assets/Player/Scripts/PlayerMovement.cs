3b5986a954bdadc6c8549039fd63ab7b67a5a731
Ascinei <alexredford.random@gmail.com>
2024-02-23 23:25:09 -0800

    Added dash to playerMovement

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -11,6 +11,23 @@ public class PlayerMovement : MonoBehaviour
     // The player
     [SerializeField] private Rigidbody2D rb;
 
+    // Dash Variables
+    private float activeMoveSpeed;
+    public float dashSpeed = 60f;
+
+    public float dashLength = .1f, dashCooldown = 1f;
+
+    private float dashCounter;
+    private float dashCoolCounter;
+
+    // End of Dash Variables
+
+    void Start()
+    {
+        activeMoveSpeed = speed;
+    }
+
+
     // Update is called once per frame
     void Update()
     { 
@@ -18,7 +35,35 @@ public class PlayerMovement : MonoBehaviour
         // UnityEngine.Vector3 PlayerMovement = UnityEngine.Quaternion.AngleAxis(45, UnityEngine.Vector3.forward) * new UnityEngine.Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         UnityEngine.Vector3 PlayerMovement = new UnityEngine.Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
         // Controls the velocity of the player.
-        rb.velocity = PlayerMovement * speed;
+        rb.velocity = PlayerMovement * activeMoveSpeed;
+
+        // Dash Checks
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if(dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if(dashCounter <= 0)
+            {
+                activeMoveSpeed = speed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+
+        // End of Dash Checks
     }
 
 }
