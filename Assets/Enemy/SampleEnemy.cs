b02224881de68e04085e79773462b1739d78ed96
Hao <120632700+hcnguyen1@users.noreply.github.com>
2024-02-25 17:03:14 -0800

    Minor fix to parent class

--- Assets/Enemy/SampleEnemy.cs
+++ Assets/Enemy/SampleEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SampleEnemy : Enemy
+public class SampleEnemy : Entity
 {
     // Start is called before the first frame update
     void Start()
