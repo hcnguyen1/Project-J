c5eddb4b972f368b4d94c79a2c8b3fd295590dc9
Ascinei <alexredford.random@gmail.com>
2024-03-06 10:10:23 -0800

    Added base code to SampleEnemy script

--- Assets/Enemy/SampleEnemy.cs
+++ Assets/Enemy/SampleEnemy.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SampleEnemy : Entity
 {
-    // Start is called before the first frame update
-    void Start()
+
+    
+    public Transform playerTransform; // Assign the player's transform in the Inspector
+
+    private void Start()
     {
-        
+        moveSpeed = 3f;
+    }
+    private void Update()
+    {
+        ChasePlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChasePlayer()
     {
-        
+        // Calculate the direction from the enemy to the player
+        Vector3 direction = playerTransform.position - transform.position;
+
+        // Normalize the direction to get a direction vector with a length of 1
+        direction.Normalize();
+
+        // Move the enemy towards the player
+        transform.position += direction * moveSpeed * Time.deltaTime;
     }
 }
